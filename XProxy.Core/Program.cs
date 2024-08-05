352506df72d7233cf75ccd409e3b9bef7d0fc4d8
Killers0992 <38152961+Killers0992@users.noreply.github.com>
2024-08-05 23:13:04 +0200

    Add tracking name

--- XProxy.Core/Program.cs
+++ XProxy.Core/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 using XProxy.Services;
 using XProxy.Shared.Models;
 
-[assembly: AssemblyVersion("1.1.7")]
+[assembly: AssemblyVersion("1.1.8")]
 
 namespace XProxy
 {
