ed14d138e996cf7efce845976df74fb6bfebb481
Killers0992 <38152961+Killers0992@users.noreply.github.com>
2024-11-16 10:25:20 +0100

    Update to .net 9

--- XProxy.Core/Serialization/YamlParser.cs
+++ XProxy.Core/Serialization/YamlParser.cs
@@ -7,15 +7,11 @@ namespace XProxy.Shared.Serialization
     public static class YamlParser
     {
         public static ISerializer Serializer { get; } = new SerializerBuilder()
-            .WithEmissionPhaseObjectGraphVisitor<CommentsObjectGraphVisitor>((EmissionPhaseObjectGraphVisitorArgs visitor) => 
-            new CommentsObjectGraphVisitor(visitor.InnerVisitor))
-                .WithTypeInspector<CommentGatheringTypeInspector>((ITypeInspector typeInspector) => 
-                new CommentGatheringTypeInspector(typeInspector))
-                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                    .DisableAliases()
-                    .IgnoreFields()
-                    .WithDefaultScalarStyle(ScalarStyle.SingleQuoted)
-                    .Build();
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .DisableAliases()
+            .IgnoreFields()
+            .WithDefaultScalarStyle(ScalarStyle.SingleQuoted)
+            .Build();
 
         public static IDeserializer Deserializer { get; } = new DeserializerBuilder()
             .WithNamingConvention(CamelCaseNamingConvention.Instance)
--- XProxy/Serialization/YamlParser.cs
+++ XProxy/Serialization/YamlParser.cs
@@ -7,15 +7,11 @@ namespace XProxy.Serialization
     public static class ConsoleYamlParser
     {
         public static ISerializer Serializer { get; } = new SerializerBuilder()
-            .WithEmissionPhaseObjectGraphVisitor<ConsoleCommentsObjectGraphVisitor>((EmissionPhaseObjectGraphVisitorArgs visitor) => 
-            new ConsoleCommentsObjectGraphVisitor(visitor.InnerVisitor))
-                .WithTypeInspector<ConsoleCommentGatheringTypeInspector>((ITypeInspector typeInspector) => 
-                new ConsoleCommentGatheringTypeInspector(typeInspector))
-                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                    .DisableAliases()
-                    .IgnoreFields()
-                    .WithDefaultScalarStyle(ScalarStyle.SingleQuoted)
-                    .Build();
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .DisableAliases()
+            .IgnoreFields()
+            .WithDefaultScalarStyle(ScalarStyle.SingleQuoted)
+            .Build();
 
         public static IDeserializer Deserializer { get; } = new DeserializerBuilder()
             .WithNamingConvention(CamelCaseNamingConvention.Instance)
