ffd0bcb6b94ad4d16ff4947c0437029f8945a7ce
Killers <killers0992@gmail.com>
2025-08-15 02:36:10 +0200

    Fix

--- XProxy/Misc/ProxyUtils.cs
+++ XProxy/Misc/ProxyUtils.cs
@@ -6,20 +6,22 @@ public static class ProxyUtils
     {
         Dictionary<ushort, Type> messages = new Dictionary<ushort, Type>();
 
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-        foreach (var assembly in assemblies)
-        {
-            var types = assembly.GetTypes();
-
-            var messageTypes = types.Where(t => typeof(NetworkMessage).IsAssignableFrom(t));
-
-            foreach (var messageType in messageTypes)
-            {
-                ushort hash = (ushort)messageType.FullName.GetStableHashCode();
-                messages.Add(hash, messageType);
-            }
-        }
+        Get(typeof(ServerConsole), ref messages);
+        Get(typeof(Mirror.Batcher), ref messages);
 
         return messages;
+    }
+
+    static void Get(Type type, ref Dictionary<ushort, Type> messages)
+    {
+        var types = type.Assembly.GetTypes();
+
+        var messageTypes = types.Where(t => typeof(NetworkMessage).IsAssignableFrom(t));
+
+        foreach (var messageType in messageTypes)
+        {
+            ushort hash = (ushort)messageType.FullName.GetStableHashCode();
+            messages.Add(hash, messageType);
+        }
     }
 }
